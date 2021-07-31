c804238f29161734e1a16093a168b80494de20fa
HYUNIL <sukhyunil@naver.com>
2021-07-31 19:27:04 +0900

    finished factory && solving fibonacci

---
+++ src/main/java/com/example/baekjoon/BaekJoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.baekjoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekJoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaekJoonApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/baekjoon/factorial/Main.java
@@ -0,0 +1,22 @@
+package com.example.baekjoon.factorial;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        int sum = function(num);
+
+        System.out.println(sum);
+    }
+    public static int function (int num) {
+        if (num <=1) {
+            return 1;
+        }else {
+            return num * function(num - 1);
+        }
+    }
+}
