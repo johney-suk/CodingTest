ea872fe8792e6c555dddc19363afd7f2759008b2
HYUNIL <sukhyunil@naver.com>
2021-08-01 16:48:44 +0900

    finished whatisStackOverFlow

---
+++ src/main/java/com/example/baekjoon/fibonacci/Main.java
@@ -0,0 +1,24 @@
+package com.example.baekjoon.fibonacci;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        int sum = fibonacci(num);
+
+        System.out.println(sum);
+    }
+    public static int fibonacci (int num) {
+        if (num == 0) {
+            return 0;
+        }else if (num <=2) {
+            return 1;
+        } else {
+            return fibonacci(num - 1)+ fibonacci(num - 2);
+        }
+    }
+}
